e7c628d8ac273c477ba038034191d413f7014faa
Mutsa Meda <mutsa.meda@gmail.com>
2020-09-03 14:52:24 -0400

    added ease transitions to navbar

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,6 +12,48 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 
+// Change navigation active link color as the user scrolls down the page
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#mainNavbar a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#mainNavbar ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
+
 
 // Close mobile menu on click - the code below might not be used
 
