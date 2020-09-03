a7c9381018f7deec540886120eacf85750779d8e
Mutsa Meda <mutsa.meda@gmail.com>
2020-09-03 14:39:54 -0400

    updated scripts.js to add nav color on scroll and made nav transparent at top of page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,17 @@
+// Change navigation color on scroll
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    jQuery('#mainNavbar').css("background-color", "transparent");
+    jQuery(window).scroll(function() {
+        if (jQuery(window).scrollTop() > 50) {
+            jQuery('#mainNavbar').css("background-color", "rgba(214, 171, 195, 0.95)");              
+        } else {
+            jQuery('mainNavbar').css("background-color", "rgba(255, 255, 255, 0.2)");
+        }
+    });
+});
+
+
 
 // Close mobile menu on click - the code below might not be used
 
