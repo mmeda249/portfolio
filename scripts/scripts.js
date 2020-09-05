1cd7ae3a317752376a267da517e9ed08aa05285e
Mutsa Meda <mutsa.meda@gmail.com>
2020-09-05 10:12:55 -0400

    changed card color in projects section to a light purple

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,5 @@
 // Change navigation color on scroll
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    jQuery('#mainNavbar').css("background-color", "transparent");
-    jQuery(window).scroll(function() {
-        if (jQuery(window).scrollTop() > 80) {
-            jQuery('#mainNavbar').css("background-color", "rgba(14, 7, 22, 0.95)");              
-        } else {
-            jQuery('mainNavbar').css("background-color", "rgba(255, 255, 255, 0.2)");
-        }
-    });
-});
-
 
 // Change navigation active link color as the user scrolls down the page
 
