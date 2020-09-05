de4b3eca59960e79e5b0fc9c28bc8cc801b118cb
Mutsa Meda <mutsa.meda@gmail.com>
2020-09-05 10:57:01 -0400

    added functions to open mobile menu upon user click in scripts.js, added transition animations to nav

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,29 @@
-// Change navigation color on scroll
+// Open mobile menu when user clicks
+
+var open = document.getElementById('hamburger');
+var changeIcon = true;
+
+open.addEventListener("click", function(){
+
+    var overlay = document.querySelector('.overlay');
+    var nav = document.querySelector('nav');
+    var icon = document.querySelector('.menu-toggle i');
+
+    overlay.classList.toggle("menu-open");
+    nav.classList.toggle("menu-open");
+
+    if (changeIcon) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-times");
+
+        changeIcon = false;
+    }
+    else {
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars");
+        changeIcon = true;
+    }
+});
 
 
 // Change navigation active link color as the user scrolls down the page
