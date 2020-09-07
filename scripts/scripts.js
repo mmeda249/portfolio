ad3d25dcd14876316fbf01d0296b086f480b440e
Mutsa Meda <mutsa.meda@gmail.com>
2020-09-07 02:17:02 -0400

    increased top padding of hero text

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,7 @@ const header = document.querySelector('.main-header');
 
 window.addEventListener('scroll', () => {
   const scrollPos = window.scrollY;
-  if(scrollPos > 10) {
+  if(scrollPos > 70) {
     header.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
