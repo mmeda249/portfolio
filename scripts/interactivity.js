0a65ec3f989b2929a32dc1c8fe727a4b33b30281
Mutsa Meda <mutsa.meda@gmail.com>
2020-09-24 12:19:55 -0400

    adjusted padding for nav-links and adjusted responsive layout font size for hero section

--- scripts/interactivity.js
+++ scripts/interactivity.js
@@ -1,22 +1,8 @@
-// Navigation bar will change color when user scrolls 70px down and beyond
-
-const header = document.querySelector('.main-header');
-
-window.addEventListener('scroll', () => {
-  const scrollPos = window.scrollY;
-  if(scrollPos > 70) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-});
 
 // When the user clicks a link from the navigation toggler, the nav will collapse again
-
-// Change navigation active link color as the user scrolls down the page
-
-// Close mobile menu on click - the code below might not be used
-
+$('.navbar-nav>li>a').on('click', function(){
+  $('.navbar-collapse').collapse('hide');
+});
 
 // Back-to-Top button
 mybutton = document.getElementById("myBtn");
@@ -58,7 +44,7 @@ var status = document.getElementById("my-form-status");
   function success() {
     form.reset();
     button.style = "display: none ";
-    status.innerHTML = " &#10003 Thanks! We'll be in touch soon!";
+    status.innerHTML = " &#10003 Message sent! We'll be in touch shortly.";
   }
 
   function error() {
@@ -89,4 +75,4 @@ function ajax(method, url, data, success, error) {
     }
   };
   xhr.send(data);
-};
+}
